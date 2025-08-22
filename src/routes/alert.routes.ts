981f7703567992ee8e16132b8027880ef46864a3
Mohammad Reza Arasteh Shahrah <drsprogramming2020@gmail.com>
2025-08-22 18:07:25 +0330

    docs: document alert routes and schema

--- src/routes/alert.routes.ts
+++ src/routes/alert.routes.ts
@@ -1,11 +1,127 @@
 import { Router } from "express";
-import { getAlerts, getAlertById, createAlert, updateAlert, deleteAlert } from "../controllers/alert.controller";
+import {
+  getAlerts,
+  getAlertById,
+  createAlert,
+  updateAlert,
+  deleteAlert,
+} from "../controllers/alert.controller";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /api/alerts:
+ *   get:
+ *     tags:
+ *       - Alerts
+ *     summary: Get all alerts
+ *     responses:
+ *       200:
+ *         description: List of alerts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Alert'
+ *       404:
+ *         description: Alerts not found
+ *       500:
+ *         description: Internal server error
+ *   post:
+ *     tags:
+ *       - Alerts
+ *     summary: Create an alert
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Alert'
+ *     responses:
+ *       201:
+ *         description: Alert created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Alert'
+ *       500:
+ *         description: Internal server error
+ */
 router.get("/", getAlerts);
-router.get("/:id", getAlertById);
 router.post("/", createAlert);
+
+/**
+ * @openapi
+ * /api/alerts/{id}:
+ *   get:
+ *     tags:
+ *       - Alerts
+ *     summary: Get alert by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Alert retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Alert'
+ *       404:
+ *         description: Alert not found
+ *       500:
+ *         description: Internal server error
+ *   put:
+ *     tags:
+ *       - Alerts
+ *     summary: Update an alert
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Alert'
+ *     responses:
+ *       200:
+ *         description: Alert updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Alert'
+ *       404:
+ *         description: Alert not found
+ *       500:
+ *         description: Internal server error
+ *   delete:
+ *     tags:
+ *       - Alerts
+ *     summary: Delete an alert
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: Alert deleted
+ *       404:
+ *         description: Alert not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/:id", getAlertById);
 router.put("/:id", updateAlert);
 router.delete("/:id", deleteAlert);
 
