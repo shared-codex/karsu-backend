4fe2154684958640bf14a22f0646ae7a68229d9f
Mohammad Reza Arasteh Shahrah <drsprogramming2020@gmail.com>
2025-08-22 18:46:42 +0330

    chore: add swagger validation

---
+++ scripts/swagger-validate.ts
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerConfig from '../src/swagger/config';
+
+const baseDir = path.join(__dirname, '..', 'src', 'routes');
+
+function processExamples(obj: any) {
+  if (obj && typeof obj === 'object') {
+    if (obj.examples && typeof obj.examples === 'object') {
+      for (const key of Object.keys(obj.examples)) {
+        const ex = obj.examples[key];
+        if (ex && typeof ex === 'object') {
+          if ('$ref' in ex && typeof ex.$ref === 'string') {
+            const refPath = path.resolve(baseDir, ex.$ref);
+            const data = JSON.parse(fs.readFileSync(refPath, 'utf8'));
+            obj.examples[key] = { value: data };
+          } else if (!('value' in ex)) {
+            obj.examples[key] = { value: ex };
+          }
+        }
+      }
+    }
+    for (const val of Object.values(obj)) {
+      processExamples(val);
+    }
+  }
+}
+
+const outputPath = path.join(baseDir, 'swagger.json');
+const spec = swaggerJsdoc(swaggerConfig);
+processExamples(spec);
+fs.writeFileSync(outputPath, JSON.stringify(spec, null, 2));
+
+try {
+  execSync(`npx swagger-cli validate ${outputPath}`, { stdio: 'inherit' });
+} finally {
+  fs.unlinkSync(outputPath);
+}
+
