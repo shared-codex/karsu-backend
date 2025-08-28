5568acfca9ad5c61c85e2ed557120952c0c0e394
shared-codex <sharedgpt.developer@gmail.com>
2025-08-28 21:58:37 +0330

    feat: centralize config with zod

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import "dotenv/config";
 import "reflect-metadata";
 import app from "./app";
 import { AppDataSource } from "./database";
+import { config } from "./config";
 
-const PORT = process.env.PORT || 3000;
+const PORT = config.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 AppDataSource.initialize()
   .then(() => {
-      // TODO: later i should have app init here
+    // TODO: later i should have app init here
   })
   .catch((error) => {
     console.error("Error during Data Source initialization", error);
